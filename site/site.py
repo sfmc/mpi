446810b08985050db3818a61bb7063666b40f1d0
sfmc <scottfmcleod@gmail.com>
2017-12-21 12:00:37 -0600

    intermediate

--- site/site.py
+++ site/site.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 #https://www.raspberrypi-spy.co.uk/2017/07/create-a-basic-python-web-server-with-flask/
 
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def index():
-	data=[]
-	return render_template('index.html', data=data)
+	events = ["1", "2", "abc"]
+	#data={'events':events}
+	data = []
+	if request.method == 'POST':
+		return "HELLO"
+	else:
+		return render_template('index.html', data=data)
   
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',debug=True)
