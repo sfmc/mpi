5fc3384247d0c23ce23b00f5e11a20190e8e9f11
sfmc <scottfmcleod@gmail.com>
2017-12-22 08:49:12 -0600

    some html styling

--- site/site.py
+++ site/site.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
+from scheduled_lasing import ScheduledLasing
+
 app = Flask(__name__)
 #https://www.raspberrypi-spy.co.uk/2017/07/create-a-basic-python-web-server-with-flask/
 
 @app.route("/", methods = ['POST', 'GET'])
 def index():
-	events = ["1", "2", "abc"]
-	data={'events':events}
+	scheduled_lasings = []
+	scheduled_lasings.append(ScheduledLasing())
+	scheduled_lasings.append(ScheduledLasing())
+	scheduled_lasings.append(ScheduledLasing())
+	data={'scheduled_lasings':scheduled_lasings}
 	if request.method == 'POST':
 		return "HELLO"
 	else:
