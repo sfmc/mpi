434a828c9373a7ff211e5521cf11fa9629d2ee6d
sfmc <scottfmcleod@gmail.com>
2017-12-23 17:57:30 -0600

    actually adding files

---
+++ site/scheduled_lasing.py
@@ -0,0 +1,34 @@
+import time, datetime, uuid, json
+
+
+def custom_converter(o):
+	if isinstance(o, datetime.datetime):
+		return o.__str__()
+	elif isinstance(o, uuid.UUID):
+		return o.__str__()
+	elif isinstance(o, datetime.time):
+		return o.__str__()
+	else:
+		raise Exception("can't serialize type " + type(o).__name__)
+
+
+class ScheduledLasing():
+	index = 0
+
+	def __init__(self):
+		ScheduledLasing.index += 1
+		#self.id = ScheduledLasing.index
+		self.id = uuid.uuid4()
+		self.DaysOfWeek = ['mon']
+		self.StartTime = datetime.time(14,0,0,0)
+		self.RecordVideo = False
+	
+	@staticmethod
+	def to_file(filename, scheduled_lasings):
+		json_data = [s.__dict__ for s in scheduled_lasings]
+		with open(filename, 'w') as outfile:
+			json.dump({'scheduled_lasings':json_data}, outfile, default = custom_converter, indent=4)
+	
+	@staticmethod
+	def from_file(filename):
+		pass
