435e9373cc3f8a263b80ec0e8f7b3133e1f5ae98
sfmc <scottfmcleod@gmail.com>
2017-12-23 17:44:00 -0600

    intermediate

--- site/site.py
+++ site/site.py
@@ -13,6 +13,7 @@ def index():
 	data={'scheduled_lasings':scheduled_lasings}
 	if request.method == 'POST':
 		if request.form['submit'] == 'delete':
+			ScheduledLasing.to_file('data.txt', scheduled_lasings)
 			return "Delete: " + request.form['scheduled_lasing_to_delete']
 		else:
 			return "HELLO"
