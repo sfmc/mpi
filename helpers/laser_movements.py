2060b64339dba9f830cbfff510d5a6697ace483c
sfmc <scottfmcleod@gmail.com>
2017-12-28 21:25:40 -0600

    some logic:

--- helpers/laser_movements.py
+++ helpers/laser_movements.py
@@ -4,11 +4,18 @@ import interfaces.servo_hat
 import helpers.utilities
 
 def draw_circle():
-	for a in helpers.utilities.drange(0, 6.28, 0.1):
-		time.sleep(0.05)
-		pan = 60*math.sin(a)
-		tilt = 60*math.cos(a)
+	time_step = 0.005
+	pan_period = 4
+	tilt_period = 2
+	pan_steps_per_period = pan_period / time_step
+	tilt_steps_per_period = tilt_period / time_step
+	for i in range(0, int(pan_steps_per_period)):
+		pan_angle = 6.282 * i / pan_steps_per_period
+		tilt_angle = 6.282 * i / tilt_steps_per_period
+		pan = 120*math.sin(pan_angle)
+		tilt = 10+40*math.sin(tilt_angle)
 		interfaces.servo_hat.set_laser_angles(pan,tilt)
+		time.sleep(time_step)
 	#return
 	#interfaces.servo_hat.set_laser_angles(50,0)
 	#time.sleep(0.5)
